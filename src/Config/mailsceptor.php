4d417ec37c24a51ed71e7288134afe885b179058
Stephen Lake <stephen@closurecode.com>
2018-10-23 12:48:50 +0200

    Clean up config

--- src/Config/mailsceptor.php
+++ src/Config/mailsceptor.php
@@ -4,6 +4,18 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Redirect
+    |--------------------------------------------------------------------------
+    |
+    | You can redirect outgoing emails to an alternative or additional email
+    | address using the below. When the above 'proceeedAfterHooks' is set to
+    | false, all outgoing emails will *only* be sent to this address.
+    |
+    */
+    'redirect' => env('MAILSCEPTOR_REDIRECT_DESTINATION', false),
+
+    /*
+    |--------------------------------------------------------------------------
     | Database Storage
     |--------------------------------------------------------------------------
     |
@@ -13,21 +25,7 @@ return [
     | Comment out the following line or set to false to disable.
     |
     */
-
     'model' => \Mailsceptor\Models\Email::class,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Redirect
-    |--------------------------------------------------------------------------
-    |
-    | You can redirect outgoing emails to an alternative or additional email
-    | address using the below. When the above 'proceeedAfterHooks' is set to
-    | false, all outgoing emails will *only* be sent to this address.
-    |
-    */
-
-    'redirect' => env('MAILSCEPTOR_REDIRECT_DESTINATION', false),
 
     /*
     |--------------------------------------------------------------------------
